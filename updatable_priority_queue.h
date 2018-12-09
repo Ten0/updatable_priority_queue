fcefa5d1e3142a7c3d536ba8e8a425bca1e666c2
Thomas <thomas.bessou@hotmail.fr>
2018-12-09 23:25:10 +0100

    `push` and `update` operations on the priority queue now return `true` if and only if the operation has changed the state of the priority queue. This makes algorithms such as dijkstra easier to implement.

--- updatable_priority_queue.h
+++ updatable_priority_queue.h
@@ -52,12 +52,14 @@ namespace better_priority_queue {
 					return ret;
 				}
 
-				/** Sets the priority for the given key. If not present, it will be added, otherwise it will be updated */
-				void set(const Key& key, const Priority& priority, bool only_if_higher=false) {
+				/** Sets the priority for the given key. If not present, it will be added, otherwise it will be updated
+				 *  Returns true if the priority was changed.
+				 * */
+				bool set(const Key& key, const Priority& priority, bool only_if_higher=false) {
 					if(key < id_to_heappos.size() && id_to_heappos[key] >= 0) // This key is already in the pQ
-						update(key, priority, only_if_higher);
+						return update(key, priority, only_if_higher);
 					else
-						push(key, priority, only_if_higher);
+						return push(key, priority, only_if_higher);
 				}
 
 				std::pair<bool,Priority> get_priority(const Key& key) {
@@ -70,11 +72,13 @@ namespace better_priority_queue {
 					return {false, 0};
 				}
 
-				/** Returns true if the key was not inside, otherwise does nothing and returns false */
+				/** Returns true if the key was not inside and was added, otherwise does nothing and returns false
+				 *  If the key was remembered and only_if_unknown is true, does nothing and returns false
+				 * */
 				bool push(const Key& key, const Priority& priority, bool only_if_unknown=false) {
 					extend_ids(key);
 					if(id_to_heappos[key] < ((size_t)-2)) return false;
-					if(only_if_unknown && id_to_heappos[key] == ((size_t)-2)) return true;
+					if(only_if_unknown && id_to_heappos[key] == ((size_t)-2)) return false;
 					// otherwise we have id_to_heappos[key] = -1, unseen key
 					size_t n = heap.size();
 					id_to_heappos[key] = n; // For consistency in the case where nothing moves (early return)
@@ -83,7 +87,7 @@ namespace better_priority_queue {
 					return true;
 				}
 
-				/** Returns true if the key was already inside, otherwise does nothing and returns false */
+				/** Returns true if the key was already inside and was updated, otherwise does nothing and returns false */
 				bool update(const Key& key, const Priority& new_priority, bool only_if_higher=false) {
 					if(key >= id_to_heappos.size()) return false;
 					size_t heappos = id_to_heappos[key];
@@ -92,12 +96,14 @@ namespace better_priority_queue {
 					if(new_priority > priority) {
 						priority = new_priority;
 						sift_up(heappos);
+						return true;
 					}
 					else if(!only_if_higher && new_priority < priority) {
 						priority = new_priority;
 						sift_down(heappos);
+						return true;
 					}
-					return true;
+					return false;
 				}
 
 			private:
